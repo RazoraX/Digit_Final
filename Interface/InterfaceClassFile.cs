ada03ded23439a58e255fe6d407f2509d79f4c11
RazoraX <51958755+RazoraX@users.noreply.github.com>
2020-11-29 03:41:21 +0300

    Add files via upload

---
+++ Interface/InterfaceClassFile.cs
@@ -0,0 +1,13 @@
+﻿using RazorPageNewTest.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPageNewTest.Interface
+{
+    public interface INterfaceClassFile
+    {
+        public IEnumerable<ClassFile> AllFiles { get; }
+    }
+}
