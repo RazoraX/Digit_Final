f5c202794f308bbb317faf2e3a78ae4a909a86da
RazoraX <51958755+RazoraX@users.noreply.github.com>
2020-11-29 03:41:55 +0300

    Add files via upload

---
+++ Models/ClassFileViewModel.cs
@@ -0,0 +1,13 @@
+﻿using RazorPageNewTest.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPageNewTest.Models
+{
+    public class ClassFileViewModel
+    {
+        public IEnumerable<ClassFile> AllFile { get; set; }
+    }
+}
