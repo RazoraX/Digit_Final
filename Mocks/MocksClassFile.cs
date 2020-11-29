f19c52098bc36f5301ca3736b4341b37d09daaf3
RazoraX <51958755+RazoraX@users.noreply.github.com>
2020-11-29 03:41:39 +0300

    Add files via upload

---
+++ Mocks/MocksClassFile.cs
@@ -0,0 +1,39 @@
+﻿using RazorPageNewTest.Data;
+using RazorPageNewTest.Interface;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPageNewTest.Mocks
+{
+    public class MocksClassFile : INterfaceClassFile
+    {
+        
+        public IEnumerable<ClassFile> AllFiles
+        {
+            get
+            {
+                List<ClassFile> allFiles = new List<ClassFile>();
+                DirectoryInfo di = new DirectoryInfo(@"Ответ на обращение");
+                FileInfo[] array = di.GetFiles();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    FileInfo temp = (FileInfo)array[i];
+                    ClassFile file = new ClassFile
+                    {
+                        Id = i + 1,
+                        Name = temp.Name,
+                        FullName = temp.FullName,
+                        Agree = false
+                    };
+                    allFiles.Add(file);
+                }
+                return allFiles;
+            }
+        }
+
+        
+    }
+}
