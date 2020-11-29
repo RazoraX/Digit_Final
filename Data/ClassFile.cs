6a12e278c2197c7089fbf1b6a04e45ef38e0df3b
RazoraX <51958755+RazoraX@users.noreply.github.com>
2020-11-29 03:40:56 +0300

    Add files via upload

---
+++ Data/ClassFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPageNewTest.Data
+{
+    public class ClassFile
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public bool Agree { get; set; }
+    }
+}
